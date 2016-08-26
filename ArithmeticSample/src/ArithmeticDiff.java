dd51d77cf5c6be4afad08299d6daf0aa3d4feaf0
raji.suba <raji.suba1990@gmail.com>
2016-08-26 11:01:28 +0530

    New Sample Project

---
+++ ArithmeticSample/src/ArithmeticDiff.java
@@ -0,0 +1,17 @@
+
+public class ArithmeticDiff {
+
+	public static void main(String[] args) {
+		int num1 = 100;
+		int num2 = 100;
+		
+		int diff = num1-num2;
+
+		System.out.println("diff is :" + diff);
+
+	}
+
+}
+
+
+
---
+++ ArithmeticSample/src/ArithmeticSum.java
@@ -0,0 +1,19 @@
+
+public class ArithmeticSum {
+
+
+	public static void main(String[] args) {
+		int num1 = 100;
+		int num2 = 100;
+		
+		int sum = num1+num2;
+
+		
+		System.out.println("sum is :" + sum);
+
+	}
+
+}
+
+
+
